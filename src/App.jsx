39f5b95e74749d7507541ba40f9f02de9453e2c3
psantos2107 <paul.santos2107@gmail.com>
2024-04-18 16:33:40 -0700

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,24 @@ import MenuComponent from "./components/AllFoodMenu";
 import FoodDetailsComponent from "./components/FoodDetails";
 import HomeNav from "./components/HomeNav";
 import Welcome from "./components/Welcome";
-import Header from "./components/NavBar";
+import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <Header />
-      <Router>
-        <div>
-          <HomeNav /> {/* Render HomeNav component for navigation */}
-          <Routes>
-            <Route path="/" element={<Welcome />} />{" "}
-            {/* Render WelcomePage for the home route */}
-            <Route path="/food" element={<MenuComponent />} />{" "}
-            {/* Render MenuComponent for the menu route */}
-            <Route path="/food/:id" element={<FoodDetailsComponent />} />{" "}
-            {/* Render FoodDetailsComponent for the food details route */}
-          </Routes>
-        </div>
-      </Router>
+      <NavBar />
+      <div>
+        <HomeNav /> {/* Render HomeNav component for navigation */}
+        <Routes>
+          <Route path="/" element={<Welcome />} />{" "}
+          {/* Render WelcomePage for the home route */}
+          <Route path="/food" element={<MenuComponent />} />{" "}
+          {/* Render MenuComponent for the menu route */}
+          <Route path="/food/:id" element={<FoodDetailsComponent />} />{" "}
+          {/* Render FoodDetailsComponent for the food details route */}
+        </Routes>
+      </div>
       <Footer />
     </>
   );
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import { Link } from "react-router-dom"
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <div className="bg-black py-6 justify-between flex fixed w-full top-0 left-0">
-        <div className='text-white mx-4'>
-            Order Eatz
-        </div>
-        <div className='text-white'>
-            <Link className='mx-4' to="/home">Home</Link>
-            <Link className='mx-4' to="/user"> My Profile</Link>
-            <Link className='mx-4' to="/order">Order Now</Link>
-        </div>
+      <div className="text-white ml-4 text-2xl font-bold">OrderEatz</div>
+      <div className="text-white">
+        <Link className="mx-4" to="/home">
+          Home
+        </Link>
+        <Link className="mx-4" to="/user">
+          {" "}
+          My Profile
+        </Link>
+        <Link className="mx-4" to="/order">
+          Order Now
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
