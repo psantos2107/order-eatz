140e85503fa69d3eb383818c7e9421ef80969131
psantos2107 <paul.santos2107@gmail.com>
2024-04-18 15:56:38 -0700

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,29 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MenuComponent from './components/AllFoodMenu';
-import FoodDetailsComponent from './components/FoodDetails';
-import HomeNav from './components/HomeNav';
-import Welcome from './components/Welcome'; 
-import Header from "./components/NavBar"
-import Footer from "./components/Footer"
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MenuComponent from "./components/AllFoodMenu";
+import FoodDetailsComponent from "./components/FoodDetails";
+import HomeNav from "./components/HomeNav";
+import Welcome from "./components/Welcome";
+import Header from "./components/NavBar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-  <>
-    <Header />
-      <Router>
+    <>
+      <Header />
       <div>
         <HomeNav /> {/* Render HomeNav component for navigation */}
         <Routes>
-          <Route path="/" element={<Welcome />} /> {/* Render WelcomePage for the home route */}
-          <Route path="/food" element={<MenuComponent />} /> {/* Render MenuComponent for the menu route */}
-          <Route path="/food/:id" element={<FoodDetailsComponent />} /> {/* Render FoodDetailsComponent for the food details route */}
+          <Route path="/" element={<Welcome />} />{" "}
+          {/* Render WelcomePage for the home route */}
+          <Route path="/food" element={<MenuComponent />} />{" "}
+          {/* Render MenuComponent for the menu route */}
+          <Route path="/food/:id" element={<FoodDetailsComponent />} />{" "}
+          {/* Render FoodDetailsComponent for the food details route */}
         </Routes>
       </div>
-    </Router>
-    <Footer />
-  </>
+      <Footer />
+    </>
   );
 }
 
