e0f040d0bb0d518c62b515b2c92d7c86cd31f064
spookyreaper <brandon.alvarado9991@gmail.com>
2024-04-27 10:44:54 -0400

    add comments

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -56,7 +56,6 @@ function UserProfile() {
     return <div>Loading...</div>;
   }
   
-
   // JSX to render user profile information
   return (
     <div className="max-w-4xl mx-auto mt-10 p-8 bg-white shadow-lg rounded-lg text-center">
